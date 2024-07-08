76375c81b8fba75e5d47d2280c0ce11ecc0abc76
RyeinKim <ryein6896@gmail.com>
2024-07-08 15:50:30 +0900

    [fix] authUtil.js 코드 리팩토링

--- util/authUtil.js
+++ util/authUtil.js
@@ -1,9 +1,7 @@
 const dbConnect = require('../database/index.js');
+const { STATUS_CODE, STATUS_MESSAGE } = require('./constant/httpStatusCode');
 
 const isLoggedIn = async (req, res, next) => {
-    const { method, url } = req;
-    console.log(`[isLoggedIn] method: ${method}, url: ${url}`);
-
     const { session } = req.headers;
     const userId =
         req.headers.userid && !Number.isNaN(req.headers.userid)
@@ -11,16 +9,15 @@ const isLoggedIn = async (req, res, next) => {
             : null;
 
     if (!userId) {
-        return res.status(401).json({
-            status: 401,
-            message: 'required_authorization',
-            data: null,
-        });
+        const error = new Error(STATUS_MESSAGE.REQUIRED_AUTHORIZATION);
+        error.status = STATUS_CODE.UNAUTHORIZED;
+        throw error;
     }
 
     try {
         const userSessionData = await dbConnect.query(
-            `SELECT session_id FROM user_table WHERE user_id = ${userId};`,
+            `SELECT session_id FROM user_table WHERE user_id = ?;`,
+            [userId],
             res,
         );
 
@@ -29,21 +26,14 @@ const isLoggedIn = async (req, res, next) => {
             userSessionData.length === 0 ||
             session !== userSessionData[0].session_id
         ) {
-            return res.status(401).json({
-                status: 401,
-                message: 'required_authorization',
-                data: null,
-            });
+            const error = new Error(STATUS_MESSAGE.REQUIRED_AUTHORIZATION);
+            error.status = STATUS_CODE.UNAUTHORIZED;
+            throw error;
         }
 
         return next();
     } catch (error) {
-        console.error(error);
-        return res.status(500).json({
-            status: 500,
-            message: 'internal_server_error',
-            data: null,
-        });
+        next(error);
     }
 };
 
--- util/constant/httpStatusCode.js
+++ util/constant/httpStatusCode.js
@@ -13,14 +13,22 @@ const STATUS_CODE = {
 const STATUS_MESSAGE = {
     REQUIRED_EMAIL: 'required_email',
     REQUIRED_PASSWORD: 'required_password',
+    REQUIRED_AUTHORIZATION: 'required_authorization',
+
     INVALID_EMAIL_OR_PASSWORD: 'invalid_email_or_password',
-    LOGIN_SUCCESS: 'login_success',
-    INVALID_EMAIL: 'invaild_email',
+    INVALID_EMAIL: 'invalid_email',
     INVALID_NICKNAME: 'invalid_nickname',
     INVALID_PASSWORD: 'invalid_password',
+    INVALID_USER_ID: 'invalid_user_id',
+
     ALREADY_EXIST_EMAIL: 'already_exist_email',
+    NOT_FOUND_USER: 'not_found_user',
+
     SIGNUP_FAILED: 'signup_failed',
+
+    LOGIN_SUCCESS: 'login_success',
     SIGNUP_SUCCESS: 'signup_success',
+
     INTERNAL_SERVER_ERROR: 'internal_server_error',
 };
 
