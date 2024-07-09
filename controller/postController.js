9e4bc199494f352613e41e1c7fb57920b45563ee
RyeinKim <ryein6896@gmail.com>
2024-07-09 16:56:53 +0900

    [fix] post 관련 코드 리팩토링

--- controller/postController.js
+++ controller/postController.js
@@ -7,39 +7,42 @@ const {
 
 /**
  * 게시글 작성
- * 파일 업로드
  * 게시글 목록 조회
  * 게시글 상세 조회
  * 게시글 수정
+ * 게시글 삭제
  */
 
 // 게시글 작성
 exports.writePost = async (request, response, next) => {
+    const { userid: userId } = request.headers;
+    const { postTitle, postContent, attachFilePath } = request.body;
+
+    if (!postTitle) {
+        const error = new Error(STATUS_MESSAGE.INVALID_POST_TITLE);
+        error.status = STATUS_CODE.BAD_REQUEST;
+        throw error;
+    }
+
+    if (postTitle.length > 26) {
+        const error = new Error(STATUS_MESSAGE.INVALID_POST_TITLE_LENGTH);
+        error.status = STATUS_CODE.BAD_REQUEST;
+        throw error;
+    }
+
+    if (!postContent) {
+        const error = new Error(STATUS_MESSAGE.INVALID_POST_CONTENT);
+        error.status = STATUS_CODE.BAD_REQUEST;
+        throw error;
+    }
+
+    if (postContent.length > 1500) {
+        const error = new Error(STATUS_MESSAGE.INVALID_POST_CONTENT_LENGHT);
+        error.status = STATUS_CODE.BAD_REQUEST;
+        throw error;
+    }
+
     try {
-        const { postTitle, postContent, attachFilePath } = request.body;
-        const userId = request.headers.userid;
-
-        if (!postTitle) {
-            const error = new Error(STATUS_MESSAGE.INVALID_POST_TITLE);
-            error.status = STATUS_CODE.BAD_REQUEST;
-            throw error;
-        }
-        if (postTitle.length > 26) {
-            const error = new Error(STATUS_MESSAGE.INVALID_POST_TITLE_LENGTH);
-            error.status = STATUS_CODE.BAD_REQUEST;
-            throw error;
-        }
-        if (!postContent) {
-            const error = new Error(STATUS_MESSAGE.INVALID_POST_CONTENT);
-            error.status = STATUS_CODE.BAD_REQUEST;
-            throw error;
-        }
-        if (postContent.length > 1500) {
-            const error = new Error(STATUS_MESSAGE.INVALID_POST_CONTENT_LENGHT);
-            error.status = STATUS_CODE.BAD_REQUEST;
-            throw error;
-        }
-
         const requestData = {
             userId,
             postTitle,
@@ -69,134 +72,18 @@ exports.writePost = async (request, response, next) => {
         next(error);
     }
 };
-/*exports.writePost = async (request, response) => {
-    try {
-        if (request.attachFilePath === undefined) request.attachFilePath = null;
-        if (!request.body.postTitle)
-            return response.status(400).json({
-                status: 400,
-                message: 'invalid_post_title',
-                data: null,
-            });
-        if (request.body.postTitle.length > 26)
-            return response.status(400).json({
-                status: 400,
-                message: 'invalid_post_title_length',
-                data: null,
-            });
-        if (!request.body.postContent)
-            return response.status(400).json({
-                status: 400,
-                message: 'invalid_post_content',
-                data: null,
-            });
-        if (request.body.postContent.length > 1500)
-            return response.status(400).json({
-                status: 400,
-                message: 'invalid_post_content_length',
-                data: null,
-            });
-
-        const { postTitle, postContent, attachFilePath } = request.body;
-        const userId = request.headers.userid;
-
-        const requestData = {
-            userId: mysql.escape(userId),
-            postTitle: mysql.escape(postTitle),
-            postContent: mysql.escape(postContent),
-            attachFilePath:
-                attachFilePath === null ? null : mysql.escape(attachFilePath),
-        };
-        const results = await postModel.writePlainPost(requestData, response);
-
-        if (!results || results === null)
-            return response.status(500).json({
-                status: 500,
-                message: 'failed_to_write_post',
-                data: null,
-            });
-
-        if (attachFilePath != null) {
-            const reqFileData = {
-                userId: mysql.escape(userId),
-                postId: results.insertId,
-                filePath: mysql.escape(attachFilePath),
-            };
-
-            const resFileData = await postModel.uploadFile(
-                reqFileData,
-                response,
-            );
-            results.filePath = resFileData;
-        }
-
-        return response.status(201).json({
-            status: 201,
-            message: 'write_post_success',
-            data: results,
-        });
-    } catch (error) {
-        console.error(error);
-        return response.status(500).json({
-            status: 500,
-            message: 'internal_server_error',
-            data: null,
-        });
-    }
-};
-
-// 파일 업로드
-exports.uploadFile = async (request, response) => {
-    try {
-        if (!request.filePath)
-            return response.status(400).json({
-                status: 400,
-                message: 'invalid_file_path',
-                data: null,
-            });
-
-        const { userId, postId, filePath } = request.body;
-
-        const requestData = {
-            userId,
-            postId,
-            filePath,
-        };
-        const results = await postModel.uploadFile(requestData, response);
-
-        if (!results || results === null)
-            return response.status(500).json({
-                status: 500,
-                message: 'internal_server_error',
-                data: null,
-            });
-
-        return response.status(201).json({
-            status: 201,
-            message: null,
-            data: results,
-        });
-    } catch (error) {
-        console.error(error);
-        return response.status(500).json({
-            status: 500,
-            message: 'internal_server_error',
-            data: null,
-        });
-    }
-};*/
 
 // 게시글 목록 조회
 exports.getPosts = async (request, response, next) => {
+    const { offset, limit } = request.query;
+
+    if (!offset || !limit) {
+        const error = new Error(STATUS_MESSAGE.INVALID_OFFSET_OR_LIMIT);
+        error.status = STATUS_CODE.BAD_REQUEST;
+        throw error;
+    }
+
     try {
-        const { offset, limit } = request.query;
-
-        if (!offset || !limit) {
-            const error = new Error(STATUS_MESSAGE.INVALID_OFFSET_OR_LIMIT);
-            error.status = STATUS_CODE.BAD_REQUEST;
-            throw error;
-        }
-
         const requestData = {
             offset: parseInt(offset, 10),
             limit: parseInt(limit, 10),
@@ -218,56 +105,18 @@ exports.getPosts = async (request, response, next) => {
         next(error);
     }
 };
-/*exports.getPosts = async (request, response) => {
-    try {
-        if (!request.query.offset || !request.query.limit)
-            return response.status(400).json({
-                status: 400,
-                message: 'invalid_offset_or_limit',
-                data: null,
-            });
-
-        const { offset, limit } = request.query;
-
-        const requestData = {
-            offset,
-            limit,
-        };
-        const results = await postModel.getPosts(requestData, response);
-
-        if (!results || results === null)
-            return response.status(404).json({
-                status: 404,
-                message: 'not_a_single_post',
-                data: null,
-            });
-
-        return response.status(200).json({
-            status: 200,
-            message: null,
-            data: results,
-        });
-    } catch (error) {
-        console.error(error);
-        return response.status(500).json({
-            status: 500,
-            message: 'internal_server_error',
-            data: null,
-        });
-    }
-};*/
 
 // 게시글 상세 조회
 exports.getPost = async (request, response, next) => {
+    const { post_id: postId } = request.params;
+
+    if (!postId) {
+        const error = new Error(STATUS_MESSAGE.INVALID_POST_ID);
+        error.status = STATUS_CODE.BAD_REQUEST;
+        throw error;
+    }
+
     try {
-        const postId = request.params.post_id;
-
-        if (!postId) {
-            const error = new Error(STATUS_MESSAGE.INVALID_POST_ID);
-            error.status = STATUS_CODE.BAD_REQUEST;
-            throw error;
-        }
-
         const requestData = {
             postId,
         };
@@ -291,23 +140,23 @@ exports.getPost = async (request, response, next) => {
 
 // 게시글 수정
 exports.updatePost = async (request, response, next) => {
+    const { post_id: postId } = request.params;
+    const { userid: userId } = request.headers;
+    const { postTitle, postContent, attachFilePath } = request.body;
+
+    if (!postId) {
+        const error = new Error(STATUS_MESSAGE.INVALID_POST_ID);
+        error.status = STATUS_CODE.BAD_REQUEST;
+        throw error;
+    }
+
+    if (postTitle.length > 26) {
+        const error = new Error(STATUS_MESSAGE.INVALID_POST_TITLE_LENGTH);
+        error.status = STATUS_CODE.BAD_REQUEST;
+        throw error;
+    }
+
     try {
-        const postId = request.params.post_id;
-        const userId = request.headers.userid;
-        const { postTitle, postContent, attachFilePath } = request.body;
-
-        if (!postId) {
-            const error = new Error(STATUS_MESSAGE.INVALID_POST_ID);
-            error.status = STATUS_CODE.BAD_REQUEST;
-            throw error;
-        }
-
-        if (postTitle.length > 26) {
-            const error = new Error(STATUS_MESSAGE.INVALID_POST_TITLE_LENGTH);
-            error.status = STATUS_CODE.BAD_REQUEST;
-            throw error;
-        }
-
         const requestData = {
             postId,
             userId,
@@ -332,71 +181,18 @@ exports.updatePost = async (request, response, next) => {
         next(error);
     }
 };
-/*exports.updatePost = async (request, response) => {
-    try {
-        if (!request.params.post_id)
-            return response.status(400).json({
-                status: 400,
-                message: 'invalid_post_id',
-                data: null,
-            });
-
-        if (request.body.postTitle.length > 26)
-            return response.status(400).json({
-                status: 400,
-                message: 'invalid_post_title_length',
-                data: null,
-            });
-
-        const postId = request.params.post_id;
-        const userId = request.headers.userid;
-        const { postTitle, postContent, attachFilePath } = request.body;
-
-        const requestData = {
-            postId: mysql.escape(postId),
-            userId: mysql.escape(userId),
-            postTitle: mysql.escape(postTitle),
-            postContent: mysql.escape(postContent),
-            attachFilePath:
-                attachFilePath === undefined
-                    ? null
-                    : mysql.escape(attachFilePath),
-        };
-        const results = await postModel.updatePost(requestData, response);
-
-        if (!results || results === null)
-            return response.status(404).json({
-                status: 404,
-                message: 'not_a_single_post',
-                data: null,
-            });
-
-        return response.status(200).json({
-            status: 200,
-            message: 'update_post_success',
-            data: results,
-        });
-    } catch (error) {
-        console.error(error);
-        return response.status(500).json({
-            status: 500,
-            message: 'internal_server_error',
-            data: null,
-        });
-    }
-};*/
 
 // 게시글 삭제
 exports.softDeletePost = async (request, response, next) => {
+    const { post_id: postId } = request.params;
+
+    if (!postId) {
+        const error = new Error(STATUS_MESSAGE.INVALID_POST_ID);
+        error.status = STATUS_CODE.BAD_REQUEST;
+        throw error;
+    }
+
     try {
-        const postId = request.params.post_id;
-
-        if (!postId) {
-            const error = new Error(STATUS_MESSAGE.INVALID_POST_ID);
-            error.status = STATUS_CODE.BAD_REQUEST;
-            throw error;
-        }
-
         const requestData = {
             postId,
         };
@@ -417,40 +213,3 @@ exports.softDeletePost = async (request, response, next) => {
         return next(error);
     }
 };
-/*exports.softDeletePost = async (request, response) => {
-    try {
-        if (!request.params.post_id)
-            return response.status(400).json({
-                status: 400,
-                message: 'invalid_post_id',
-                data: null,
-            });
-
-        const postId = request.params.post_id;
-
-        const requestData = {
-            postId: mysql.escape(postId),
-        };
-        const results = await postModel.softDeletePost(requestData, response);
-
-        if (!results || results === null)
-            return response.status(404).json({
-                status: 404,
-                message: 'not_a_single_post',
-                data: null,
-            });
-
-        return response.status(200).json({
-            status: 200,
-            message: 'delete_post_success',
-            data: null,
-        });
-    } catch (error) {
-        console.error(error);
-        return response.status(500).json({
-            status: 500,
-            message: 'internal_server_error',
-            data: null,
-        });
-    }
-};*/
--- model/postModel.js
+++ model/postModel.js
@@ -1,8 +1,15 @@
 const dbConnect = require('../database/index.js');
 const { STATUS_MESSAGE } = require('../util/constant/httpStatusCode');
 
-// 게시글 작성 (일반 게시글)
-// 게시글 작성 (일반 게시글)
+/**
+ * 게시글 작성
+ * 게시글 목록 조회
+ * 게시글 상세 조회
+ * 게시글 수정
+ * 게시글 삭제
+ */
+
+// 게시글 작성
 exports.writePost = async requestData => {
     const { userId, postTitle, postContent, attachFilePath } = requestData;
 
@@ -58,47 +65,6 @@ exports.writePost = async requestData => {
 
     return writePostResults;
 };
-/*exports.writePlainPost = async (requestData, response) => {
-    const { userId, postTitle, postContent } = requestData;
-
-    const nicknameSql = `
-    SELECT nickname FROM user_table
-    WHERE user_id = ${userId} AND deleted_at IS NULL;
-    `;
-    const nicknameResults = await dbConnect.query(nicknameSql, response);
-    console.log(nicknameResults);
-
-    const writePostSql = `
-    INSERT INTO post_table
-    (user_id, nickname, post_title, post_content)
-    VALUES (${userId}, '${nicknameResults[0].nickname}', ${postTitle}, ${postContent});
-    `;
-
-    const writePostResults = await dbConnect.query(writePostSql, response);
-    return writePostResults;
-};
-
-// 파일 업로드
-exports.uploadFile = async (requestData, response) => {
-    const { userId, postId, filePath } = requestData;
-
-    const postFilePathSql = `
-    INSERT INTO file_table
-    (user_id, post_id, file_path, file_category)
-    VALUES (${userId}, ${postId}, ${filePath}, 2);
-    `;
-
-    const postFileResults = await dbConnect.query(postFilePathSql, response);
-
-    const updatePostSql = `
-    UPDATE post_table
-    SET file_id = ${postFileResults.insertId}
-    WHERE post_id = ${postId};
-    `;
-
-    const updatePostResults = await dbConnect.query(updatePostSql, response);
-    return updatePostResults.insertId;
-};*/
 
 // 게시글 목록 조회
 exports.getPosts = async (requestData, response) => {
@@ -144,70 +110,6 @@ exports.getPosts = async (requestData, response) => {
 };
 
 // 게시글 상세 조회
-/*    const sql = `
-SELECT
-    post_table.post_id,
-    post_table.post_title,
-    post_table.post_content,
-    post_table.file_id,
-    post_table.user_id,
-    post_table.nickname,
-    post_table.created_at,
-    post_table.updated_at,
-    post_table.deleted_at,
-    CASE
-        WHEN post_table.\`like\` >= 1000000 THEN CONCAT(ROUND(post_table.\`like\` / 1000000, 1), 'M')
-        WHEN post_table.\`like\` >= 1000 THEN CONCAT(ROUND(post_table.\`like\` / 1000, 1), 'K')
-        ELSE post_table.\`like\`
-    END as \`like\`,
-    CASE
-        WHEN post_table.comment_count >= 1000000 THEN CONCAT(ROUND(post_table.comment_count / 1000000, 1), 'M')
-        WHEN post_table.comment_count >= 1000 THEN CONCAT(ROUND(post_table.comment_count / 1000, 1), 'K')
-        ELSE post_table.comment_count
-    END as comment_count,
-    CASE
-        WHEN post_table.hits >= 1000000 THEN CONCAT(ROUND(post_table.hits / 1000000, 1), 'M')
-        WHEN post_table.hits >= 1000 THEN CONCAT(ROUND(post_table.hits / 1000, 1), 'K')
-        ELSE post_table.hits
-    END as hits,
-    COALESCE(file_table.file_path, NULL) AS filePath
-FROM post_table
-LEFT JOIN file_table ON post_table.file_id = file_table.file_id
-WHERE post_table.post_id = ${postId} AND post_table.deleted_at IS NULL;
-`;
-const results = await dbConnect.query(sql, response);
-
-if (!results || results.length === 0) return null;
-
-// 조회수 증가
-const hitsSql = `
-UPDATE post_table SET hits = hits + 1 WHERE post_id = ${results[0].post_id} AND deleted_at IS NULL;
-`;
-await dbConnect.query(hitsSql, response);
-
-const userSql = `
-SELECT file_id FROM user_table WHERE user_id = ${results[0].user_id};
-`;
-const userResults = await dbConnect.query(userSql, response);
-
-if (!userResults || userResults.length === 0) return results;
-
-// 프로필 이미지 가져오기
-const profileImageSql = `
-SELECT file_path FROM file_table WHERE file_id = ${userResults[0].file_id} AND file_category = 1 AND user_id = ${results[0].user_id};
-`;
-
-const profileImageResults = await dbConnect.query(
-    profileImageSql,
-    response,
-);
-
-if (!profileImageResults || profileImageResults.length === 0)
-    return results;
-
-results[0].profileImage = profileImageResults[0].file_path;
-
-return results;*/
 exports.getPost = async (requestData, response) => {
     const { postId } = requestData;
 
@@ -346,60 +248,8 @@ exports.updatePost = async requestData => {
 
     return { ...updatePostResults, post_id: postId };
 };
-/*exports.updatePost = async (requestData, response) => {
-    const { postId, userId, postTitle, postContent, attachFilePath } =
-        requestData;
-    console.log('attachFilePath', attachFilePath);
-    const updatePostSql = `
-    UPDATE post_table
-    SET post_title = ${postTitle}, post_content = ${postContent}
-    WHERE post_id = ${postId} AND deleted_at IS NULL;
-    `;
 
-    const updatePostResults = await dbConnect.query(updatePostSql, response);
-
-    if (!updatePostResults) return null;
-
-    if (attachFilePath === null) {
-        const sql = `
-        UPDATE post_table
-        SET file_id = NULL
-        WHERE post_id = ${postId};
-        `;
-        await dbConnect.query(sql, response);
-    } else {
-        // 파일 경로 존재 여부 확인
-        const checkFilePathSql = `
-        SELECT COUNT(*) AS existing
-        FROM file_table
-        WHERE file_path = ${attachFilePath};
-        `;
-        const checkResults = await dbConnect.query(checkFilePathSql, response);
-        if (checkResults[0].existing === 0) {
-            // 파일 경로가 존재하지 않으면 새로운 파일 정보 삽입
-            const postFilePathSql = `
-            INSERT INTO file_table
-            (user_id, post_id, file_path, file_category)
-            VALUES (${userId}, ${postId}, ${attachFilePath}, 2);
-            `;
-            const postFileResults = await dbConnect.query(
-                postFilePathSql,
-                response,
-            );
-
-            // file_id 업데이트
-            const updatePostFileSql = `
-            UPDATE post_table
-            SET file_id = ${postFileResults.insertId}
-            WHERE post_id = ${postId};
-            `;
-            await dbConnect.query(updatePostFileSql, response);
-        }
-    }
-
-    return { ...updatePostResults, post_id: postId };
-};*/
-
+// 게시글 삭제
 exports.softDeletePost = async requestData => {
     const { postId } = requestData;
 
@@ -414,17 +264,3 @@ exports.softDeletePost = async requestData => {
 
     return results;
 };
-/*exports.softDeletePost = async (requestData, response) => {
-    const { postId } = requestData;
-
-    const sql = `
-    UPDATE post_table
-    SET deleted_at = NOW()
-    WHERE post_id = ${postId} AND deleted_at IS NULL;
-    `;
-    const results = await dbConnect.query(sql, response);
-
-    if (!results) return null;
-
-    return results;
-};*/
