cff9b2ad152b627bbadaf4721e5d12ac0ba9af3f
RyeinKim <ryein6896@gmail.com>
2024-08-08 14:02:02 +0900

    [fix] fix test code error

--- test/controller/userController.test.js
+++ test/controller/userController.test.js
@@ -1,7 +1,26 @@
 // test/controller/userController.test.js
 
 const request = require('supertest');
-const app = require('../../app');
+const { app, startHttpServer } = require('../../app');
+
+let server;
+
+beforeAll((done) => {
+    server = startHttpServer(0); // 포트를 0으로 설정하여 사용 가능한 포트를 자동으로 할당받음
+    server.on('listening', () => {
+        const address = server.address();
+        console.log(`Test server listening on port ${address.port}`);
+        done();
+    });
+});
+
+afterAll((done) => {
+    if (server) {
+        server.close(done);
+    } else {
+        done();
+    }
+});
 
 jest.mock('../../database/index');
 jest.mock('../../model/userModel');
@@ -12,7 +31,8 @@ describe('User Controller - loginUser', () => {
     });
 
     it('should return 400 if email is not provided', async () => {
-        const res = await request(app)
+        const address = server.address();
+        const res = await request(`http://localhost:${address.port}`) // 동적으로 할당된 포트 사용
             .post('/users/login')
             .send({ password: 'password123' });
 
