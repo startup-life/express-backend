95b3249e8c04f7bcabf27c6f203e70d4774a347c
RyeinKim <ryein6896@gmail.com>
2024-08-08 14:40:23 +0900

    .env 로드 확인을 위한 console.log 추가

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 require('dotenv').config();
 
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_DATABASE:', process.env.DB_DATABASE);
+console.log('DB_PORT:', process.env.DB_PORT);
+
 const express = require('express');
 const session = require('express-session');
 const cors = require('cors');
